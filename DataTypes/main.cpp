0f26799b440457658292ae0b0ce2ecbbe54bc11b
chadd williams <will4614@pacificu.edu>
2022-09-07 12:57:13 -0700

    add DataTypes Practice

---
+++ DataTypes/main.cpp
@@ -0,0 +1,26 @@
+//*********************************************************************
+// File name:			main.cpp
+// Author:				chadd williams
+// Date:					Sept 7, 2022
+// Class:					CS 150-02
+// Assignment:		In Class Practice
+// Purpose:				Practice with Data Types
+// Hours Worked:	.1
+//*********************************************************************
+#include <iostream>
+
+using namespace std;
+
+//*********************************************************************
+// Function:		main
+//
+// Description: Practice with Data Types
+//
+// Parameters:	None
+//
+// Returned:		EXIT_SUCCESS
+//*********************************************************************
+int main() {
+
+	return EXIT_SUCCESS;
+}
