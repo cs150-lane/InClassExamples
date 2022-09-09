9f00632704b800e2eb03fff28b64806e45be92c1
chadd williams <will4614@pacificu.edu>
2022-09-09 13:54:06 -0700

    added mealcost code

---
+++ MealCost/main.cpp
@@ -0,0 +1,18 @@
+//*********************************************************************
+// File name:			main.cpp
+// Author:				chadd williams
+// Date:					Sept 9, 2022
+// Class:					CS 150-02
+// Assignment:		In Class Practice
+// Purpose:				Practice with arithmetic operators
+// Hours Worked:	.1
+//*********************************************************************
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	return EXIT_SUCCESS;
+}
